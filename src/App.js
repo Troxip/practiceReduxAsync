d4f8449fa6399aa9b30a96979d8c6f492e5b66b6
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-26 15:19:13 +0100

    Fetch Practice

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const res = fetch(
+        "https://troxip-6e4cd-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      const data = await res.js();
+    };
+  }, [cart]);
 
   return (
     <Layout>
